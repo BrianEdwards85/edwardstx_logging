66c4107200409087f76131cc6ade5193af5ae6b9
BrianEdwards85 <brian@edwardstx.us>
2017-10-18 11:07:14 -0500

    Fixed run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/lein uberjar
 
-/usr/bin/docker build -t edwardstx/management-ui .
+/usr/bin/docker build -t edwardstx/edwardstx_logging .
 
-/usr/bin/docker run -d --restart always -p 127.0.0.1:5007:8080 -v /etc/service:/etc/service --name management-ui edwardstx/management-ui
+/usr/bin/docker run -d --restart always -v /etc/service:/etc/service --name edwardstx_logging edwardstx/edwardstx_logging
